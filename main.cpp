f1969dfca3111840d6883d3c3b1eef7ce8cad51f
Armando Antonio <arm_apocalipsis@hotmail.com>
2015-02-03 13:17:19 -0600

    function implemented to inserting new element into the list

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <string.h>
 
 using namespace std;
-struct student{
+struct student
+{
     char name[100];
     student *next;
 
@@ -12,39 +13,108 @@ struct student{
 
 student * insert(student *);
 void showAll(student *);
+void menu(student *);
+student * createNode();
+
+enum
+{
+    INSERT=1,
+    SHOW_ALL,
+    DELETE,
+    EXIT
+};
 
 int main()
 {
     student *list =NULL;
-    list=insert(list);
-    showAll(list);
+    //list=insert(list);
+    //showAll(list);
+    menu(list);
+
+    /*student *node;
+    node = createNode();
+    cout<<"Name:"<<node->name<<endl;*/
     return 0;
 }
 
-student * insert(student *list){
+//insert new item to the list
+student * insert(student *list)
+{
     student *temp =list;
     student *node;
-    node =(student *)malloc(sizeof(student));
+
+    /*node =(student *)malloc(sizeof(student));
     strcpy(node->name,"Armando");
-    node->name[8]='\0';
-    node->next=NULL;
-
-    if(list==NULL){
+    node->next=NULL;*/
+    node = createNode();
+    if(list==NULL)
+    {
         list=node;
-    }else{
-
-
     }
-return list;
+    else
+    {
+        while(temp->next){
+            temp =temp->next;
+        }
+        temp->next = node;
+    }
+    return list;
 }
-void showAll(student *list){
+
+//Show all list items
+void showAll(student *list)
+{
     student *temp = list;
-    if(list==NULL){
+    if(list==NULL)
+    {
         cout<<"Empty list"<<endl;
-    }else{
-        while(temp){
+    }
+    else
+    {
+        while(temp)
+        {
             cout<<"Name : "<<temp->name<<endl;
             temp =temp->next;
         }
     }
 }
+
+student * createNode(){
+    student *node;
+    cin.ignore();
+    node = (student *)malloc(sizeof(student));
+    cout<<"Write the students name"<<endl;
+    cin.getline(node->name,100);
+    node->next=NULL;
+    return node;
+
+}
+
+//menu
+void menu(student *list)
+{
+    int opc;
+    do
+    {
+        cout<<"1.-Insert 2.-Show all 3.-Delete 4.-Exit"<<endl;
+        cin>>opc;
+        switch(opc)
+        {
+        case INSERT:
+            cout<<"insert"<<endl;
+            list=insert(list);
+            break;
+        case SHOW_ALL:
+            cout<<"show all"<<endl;
+            showAll(list);
+            break;
+        case DELETE:
+            break;
+        default:
+            cout<<"Wrong choice"<<endl;
+        break;
+        }
+    }
+    while(opc!=EXIT);
+}
+
